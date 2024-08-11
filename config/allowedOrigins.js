9add7e8ce5adac2d5f1b42888d8500b5f76b98dc
13NILAY <nilayrathod1303@gmail.com>
2024-08-11 16:28:15 +0530

    Added Frontend Route

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins =[
     "http://localhost:5174",
     "http://localhost:5175",
     "http://127.0.0.1",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://outhouse-project.vercel.app"
    //Apna Frontend Routes daal dena   
 ];
 
