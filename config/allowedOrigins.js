46a5d9f4d37b9d20b253ae2189e8520d1728c20a
13NILAY <nilayrathod1303@gmail.com>
2024-08-11 18:50:08 +0530

    small

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,12 @@
 const allowedOrigins =[
+    "https://outhouse-project.vercel.app",
     'http://localhost:8080',
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
     "http://127.0.0.1",
     "http://localhost:3000",
-    "https://outhouse-project.vercel.app"
+    
    //Apna Frontend Routes daal dena   
 ];
 
