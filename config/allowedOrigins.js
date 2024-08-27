255cbfa91281fd6f7437d8f420edad5e1d491279
13NILAY <nilayrathod1303@gmail.com>
2024-08-27 18:38:45 +0530

    Added Origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins =[
     "https://cloathing-website-jlafsmsb2-nilay-rathods-projects.vercel.app",
+    "https://cloathing-website.vercel.app",
     "http://localhost:5173",
     "https://outhouse-project.vercel.app",
     "http://localhost:5174",
