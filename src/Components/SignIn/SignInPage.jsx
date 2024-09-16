510a06ef516af2773077299f9761d7d6e37a7557
13NILAY <nilayrathod1303@gmail.com>
2024-09-17 00:33:06 +0530

    removing UI error in login / signup

--- src/Components/SignIn/SignInPage.jsx
+++ src/Components/SignIn/SignInPage.jsx
@@ -1,51 +1,53 @@
-import React, { useCallback, useState } from 'react'
-import Login from './Login'
-import Register from './Register'
-import {motion, AnimatePresence} from 'framer-motion'
+import React, { useState } from 'react';
+import Login from './Login';
+import Register from './Register';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const SignInPage = () => {
-  
-  const [login, setLogin]= useState(false)
-  
+  const [login, setLogin] = useState(false);
+
   return (
     <>
-      <div className='flex flex-col justify-center items-center h-screen w-full' >
-        <div className='text-gray-400 w-2/5 font-headings flex flex-wrap items-center justify-around mb-8 '>
-          <button className={`text-[2.7rem] ${login?'text-typography':''} duration-200 ease-in-out border-none outline-none`}
-              onClick={()=>{setLogin(true)}}>
-            Login</button>
-          <div className='h-12 w-12 rounded-full border-4 border-gray-400 text-2xl font-bold font-texts flex justify-center items-center'>OR</div>
-          <button className={`text-[2.7rem] ${!login?'text-typography':''} duration-200 ease-in-out border-none outline-none`}
-              onClick={()=>{setLogin(false)}}>
-            Register</button>
+      <div className='flex flex-col justify-center items-center h-screen w-full bg-[#F4E1D2]'>
+        <div className='text-[#8A5D3B] w-full md:w-3/5 lg:w-2/5 font-headings flex flex-wrap items-center justify-around mb-8'>
+          <button
+            className={`text-lg md:text-2xl lg:text-[2.7rem] ${login ? 'text-[#4A2C2A]' : 'text-gray-400'} duration-200 ease-in-out border-none outline-none`}
+            onClick={() => { setLogin(true); }}>
+            Login
+          </button>
+          <div className='h-12 w-12 rounded-full border-4 border-gray-400 text-lg md:text-2xl font-bold font-texts flex justify-center items-center'>
+            OR
+          </div>
+          <button
+            className={`text-lg md:text-2xl lg:text-[2.7rem] ${!login ? 'text-[#4A2C2A]' : 'text-gray-400'} duration-200 ease-in-out border-none outline-none`}
+            onClick={() => { setLogin(false); }}>
+            Register
+          </button>
         </div>
+
         <AnimatePresence>
-          <div className='bg-white w-2/5 border-typography border flex flex-col px-5 '>
+          <div className='bg-[#F9EFE6] w-full max-w-screen-md border-[#8A5D3B] border-2 flex flex-col px-5 py-10 rounded-lg shadow-lg overflow-hidden'>
             {
-              /*FOR LOGIN PAGE */
               login &&
               (
-                <motion.div initial={{ opacity:0}} animate={{ opacity:1}} exit={{ opacity:0}} transition={{duration:2, ease:[0.4, 0, 0.2, 1]}}>
-                  <Login/>
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1, ease: [0.4, 0, 0.2, 1] }}>
+                  <Login />
                 </motion.div>
-                
               )
             }
             {
               !login &&
               (
-                <motion.div initial={{ opacity:0}} animate={{ opacity:1}} exit={{ opacity:0}} transition={{duration:2, ease:[0.4, 0, 0.2, 1]}}>
-                  <Register/>
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1, ease: [0.4, 0, 0.2, 1] }}>
+                  <Register />
                 </motion.div>
-                
               )
             }
           </div>
         </AnimatePresence>
-        
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SignInPage
+export default SignInPage;
