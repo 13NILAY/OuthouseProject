3bdf8f020f053de618c8f96298a09b50cb372dd4
13NILAY <nilayrathod1303@gmail.com>
2024-09-17 11:13:40 +0530

    Update Order Component

--- controller/orderC.js
+++ controller/orderC.js
@@ -2,11 +2,11 @@ const OrderSchema =require("../model/orderSchema");
 require("dotenv").config();
 const axios=require('axios');
 
-const createOrder= async(req,res)=>{
+const createPaymentGateway= async(req,res)=>{
     const { amount, currency } = req.body;
     console.log(req.body);
     const options = {
-        amount: amount , // amount in the smallest currency unit
+        amount: amount*100 , // amount in the smallest currency unit
         currency,
     };
 
@@ -31,15 +31,28 @@ const createOrder= async(req,res)=>{
         //     color: req.body.colors,
         //   };
         console.log(order);
+        const timestamp=order.data.created_at;
+        const date = new Date(timestamp * 1000);
+
         res.json({
             id: order.data.id,
             currency: order.data.currency,
             amount: order.data.amount,
+            created_at:date.toLocaleString()
         });
     } catch (error) {
         console.log(error)
         res.status(500).send(error.message);
     }
 }
+const createOrder=async(req,res)=>{
+    try{
+        console.log(req.body);
+        res.status(200).json("Order Created Successsfully");
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+}
 
-module.exports ={createOrder};
+module.exports ={createPaymentGateway,createOrder};
--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,6 @@ const router =express.Router();
 
 const orderC =require("../controller/orderC");
 
+router.post('/create-payment-gateway',orderC.createPaymentGateway);
 router.post('/create-order',orderC.createOrder);
-
 module.exports= router;
